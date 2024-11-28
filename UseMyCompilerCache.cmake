697cb7d5b80a172cdaaf02364175800ec43d0c43
Kene Lin <kenelin@gmail.com>
2024-11-28 12:18:48 +0800

    Add custom module to use compiler cache, part 3

--- UseMyCompilerCache.cmake
+++ UseMyCompilerCache.cmake
@@ -1,3 +1,6 @@
+# This module is used to detect if there is a compiler cache tool on host machine.
+#
+# Currently Ccache (https://ccache.dev/) is supported.
 message (STATUS "Detecting compiler cache")
 find_program (CCACHE_SHIM_EXECUTABLE_PATH
     NAMES ccache)
@@ -9,16 +12,6 @@ else ()
   # C:\>ccache --version
   # ccache version 4.10.2
   # Features: avx2 file-storage http-storage redis+unix-storage redis-storage
-  #
-  # Copyright (C) 2002-2007 Andrew Tridgell
-  # Copyright (C) 2009-2024 Joel Rosdahl and other contributors
-  #
-  # See <https://ccache.dev/credits.html> for a complete list of contributors.
-  #
-  # This program is free software; you can redistribute it and/or modify it under
-  # the terms of the GNU General Public License as published by the Free Software
-  # Foundation; either version 3 of the License, or (at your option) any later
-  # version.
   execute_process (COMMAND ${CCACHE_SHIM_EXECUTABLE_PATH} --version
       OUTPUT_VARIABLE ccache_version_output
       RESULT_VARIABLE ccache_version_exit_code
@@ -43,12 +36,6 @@ else ()
     # is gui? False
     # wait for exit? True
     # command (optional):
-    # [shim]: Command line 'ccache  --shimgen-noop'
-    # [shim]: Current process '"C:\ProgramData\chocolatey\bin\ccache.exe"'
-    # [shim]: Command line after removing process 'ccache  --shimgen-noop'
-    # [shim]: Shifting off the first argument since process didn't catch it
-    # [shim]: Arguments after removing shimgen args - ''
-    # [shim]: Arguments are ''
     execute_process (COMMAND ${CCACHE_SHIM_EXECUTABLE_PATH} --shimgen-noop
         OUTPUT_VARIABLE ccache_path_output
         RESULT_VARIABLE ccache_path_exit_code
@@ -65,7 +52,7 @@ else ()
     set (CCACHE_EXECUTABLE_PATH ${CCACHE_SHIM_EXECUTABLE_PATH})
   endif ()
 
-  message (STATUS "Detecting compiler cache - CCache ${CCACHE_EXECUTABLE_VERSION} in ${CCACHE_EXECUTABLE_PATH}")
+  message (STATUS "Detecting compiler cache - Ccache ${CCACHE_EXECUTABLE_VERSION} in ${CCACHE_EXECUTABLE_PATH}")
 
   if (MSVC)
     file (COPY_FILE ${CCACHE_EXECUTABLE_PATH} ${CMAKE_BINARY_DIR}/cl.exe
@@ -76,10 +63,10 @@ else ()
         "TrackFileAccess=false"
         "UseMultiToolTask=true"
         "DebugInformationFormat=OldStyle")
-    message (STATUS "Using CCache ${CCACHE_EXECUTABLE_VERSION} as MSVC compiler cache")
+    message (STATUS "Using Ccache ${CCACHE_EXECUTABLE_VERSION} as MSVC compiler cache")
   else ()
     set (CMAKE_C_COMPILER_LAUNCHER ${CCACHE_EXECUTABLE_PATH})
     set (CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE_EXECUTABLE_PATH})
-    message (STATUS "Using CCache ${CCACHE_EXECUTABLE_VERSION} as compiler cache")
+    message (STATUS "Using Ccache ${CCACHE_EXECUTABLE_VERSION} as compiler cache")
   endif ()
 endif ()
