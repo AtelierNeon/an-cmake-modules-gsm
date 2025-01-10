2bea366a590da3618de3f06010ab7e1a548abe76
Kene Lin <kenelin@gmail.com>
2025-01-10 13:12:04 +0800

    Update custom modules to find sibling OpenSSL (#17)

--- FindSiblingOpenSSL.cmake
+++ FindSiblingOpenSSL.cmake
@@ -1,15 +1,53 @@
 ## Check which version exists and is preferred
 if (EXISTS "${PROJECT_SOURCE_DIR}/../openssl-1_1_1/include/" AND BUILD_WITH_OPENSSL_1_1_1_PREFERRED)
+  set (_OPENSSL_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/../openssl-1_1_1/include/")
+  set (_OPENSSL_INCLUDE_DIRS
+    "${_OPENSSL_INCLUDE_DIR}"
+    "${PROJECT_BINARY_DIR}/../openssl-1_1_1/include/")
   set (_OPENSSL_1_1_1_FOUND TRUE)
 elseif (EXISTS "${PROJECT_SOURCE_DIR}/../openssl-3_0/include/" AND BUILD_WITH_OPENSSL_3_0_PREFERRED)
+  set (_OPENSSL_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/../openssl-3_0/include/")
+  set (_OPENSSL_INCLUDE_DIRS
+    "${_OPENSSL_INCLUDE_DIR}"
+    "${PROJECT_BINARY_DIR}/../openssl-3_0/include/")
   set (_OPENSSL_3_0_FOUND TRUE)
 else ()
   if (EXISTS "${PROJECT_SOURCE_DIR}/../openssl/include/")
+    set (_OPENSSL_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/../openssl/include/")
+    set (_OPENSSL_INCLUDE_DIRS
+      "${_OPENSSL_INCLUDE_DIR}"
+      "${PROJECT_BINARY_DIR}/../openssl/include/")
     set (_OPENSSL_FOUND TRUE)
   elseif (EXISTS "${PROJECT_SOURCE_DIR}/../openssl-1_1_1/include/")
+    set (_OPENSSL_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/../openssl-1_1_1/include/")
+    set (_OPENSSL_INCLUDE_DIRS
+      "${_OPENSSL_INCLUDE_DIR}"
+      "${PROJECT_BINARY_DIR}/../openssl-1_1_1/include/")
     set (_OPENSSL_1_1_1_FOUND TRUE)
   elseif (EXISTS "${PROJECT_SOURCE_DIR}/../openssl-3_0/include/")
+    set (_OPENSSL_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/../openssl-3_0/include/")
+    set (_OPENSSL_INCLUDE_DIRS
+      "${_OPENSSL_INCLUDE_DIR}"
+      "${PROJECT_BINARY_DIR}/../openssl-3_0/include/")
     set (_OPENSSL_3_0_FOUND TRUE)
+  elseif (EXISTS "${PROJECT_SOURCE_DIR}/../../openssl/include/")
+    set (_OPENSSL_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/../../openssl/include/")
+    set (_OPENSSL_INCLUDE_DIRS
+      "${_OPENSSL_INCLUDE_DIR}"
+      "${PROJECT_BINARY_DIR}/../../openssl/include/")
+    set (_OPENSSL_FOUND TRUE)
+  elseif (EXISTS "${PROJECT_SOURCE_DIR}/../../../openssl/include/")
+    set (_OPENSSL_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/../../../openssl/include/")
+    set (_OPENSSL_INCLUDE_DIRS
+      "${_OPENSSL_INCLUDE_DIR}"
+      "${PROJECT_BINARY_DIR}/../../../openssl/include/")
+    set (_OPENSSL_FOUND TRUE)
+  elseif (EXISTS "${PROJECT_SOURCE_DIR}/../../../../openssl/include/")
+    set (_OPENSSL_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/../../../../openssl/include/")
+    set (_OPENSSL_INCLUDE_DIRS
+      "${_OPENSSL_INCLUDE_DIR}"
+      "${PROJECT_BINARY_DIR}/../../../../openssl/include/")
+    set (_OPENSSL_FOUND TRUE)
   endif ()
 endif ()
 
@@ -18,9 +56,7 @@ if (_OPENSSL_1_1_1_FOUND)
   message (STATUS "Sibling OpenSSL 1.1.1 found.")
   set (OPENSSL_FOUND TRUE)
   set (OPENSSL_1_1_1_FOUND TRUE)
-  set (OPENSSL_INCLUDE_DIRS
-    "${PROJECT_SOURCE_DIR}/../openssl-1_1_1/include/"
-    "${PROJECT_BINARY_DIR}/../openssl-1_1_1/include/")
+  set (OPENSSL_INCLUDE_DIRS ${_OPENSSL_INCLUDE_DIRS})
   set (OPENSSL_CRYPTO_LIBRARY crypto)
   set (OPENSSL_SSL_LIBRARY ssl)
   set (OPENSSL_LIBRARIES
@@ -30,9 +66,7 @@ elseif (_OPENSSL_3_0_FOUND)
   message (STATUS "Sibling OpenSSL 3.0 found.")
   set (OPENSSL_FOUND TRUE)
   set (OPENSSL_3_0_FOUND TRUE)
-  set (OPENSSL_INCLUDE_DIRS
-    "${PROJECT_SOURCE_DIR}/../openssl-3_0/include/"
-    "${PROJECT_BINARY_DIR}/../openssl-3_0/include/")
+  set (OPENSSL_INCLUDE_DIRS ${_OPENSSL_INCLUDE_DIRS})
   set (OPENSSL_CRYPTO_LIBRARY crypto)
   set (OPENSSL_SSL_LIBRARY ssl)
   set (OPENSSL_LIBRARIES
@@ -41,26 +75,24 @@ elseif (_OPENSSL_3_0_FOUND)
 elseif (_OPENSSL_FOUND)
   message (STATUS "Sibling OpenSSL found.")
   set (OPENSSL_FOUND TRUE)
-  set (OPENSSL_INCLUDE_DIRS
-    "${PROJECT_SOURCE_DIR}/../openssl/include/"
-    "${PROJECT_BINARY_DIR}/../openssl/include/")
+  set (OPENSSL_INCLUDE_DIRS ${_OPENSSL_INCLUDE_DIRS})
   set (OPENSSL_CRYPTO_LIBRARY crypto)
   set (OPENSSL_SSL_LIBRARY ssl)
   set (OPENSSL_LIBRARIES
     ${OPENSSL_CRYPTO_LIBRARY}
     ${OPENSSL_SSL_LIBRARY})
-  if (EXISTS "${PROJECT_SOURCE_DIR}/../openssl/include/openssl/opensslv.h")
-    file (READ "${PROJECT_SOURCE_DIR}/../openssl/include/openssl/opensslv.h" OPENSSL_VERSION)
-    string (FIND "${OPENSSL_VERSION}" "OpenSSL 1.1.1" match_1_1_1)
+  if (EXISTS "${_OPENSSL_INCLUDE_DIR}/openssl/opensslv.h")
+    file (READ "${_OPENSSL_INCLUDE_DIR}/openssl/opensslv.h" _OPENSSL_VERSION)
+    string (FIND "${_OPENSSL_VERSION}" "OpenSSL 1.1.1" match_1_1_1)
     if (${match_1_1_1} GREATER_EQUAL 0)
       message (STATUS "Sibling OpenSSL 1.1.1 found.")
       set (OPENSSL_1_1_1_FOUND TRUE)
     endif ()
   endif ()
-  if (EXISTS "${PROJECT_SOURCE_DIR}/../openssl/VERSION.dat")
-    file (READ "${PROJECT_SOURCE_DIR}/../openssl/VERSION.dat" OPENSSL_VERSION)
-    string (FIND "${OPENSSL_VERSION}" "MAJOR=3" match_major3)
-    string (FIND "${OPENSSL_VERSION}" "MINOR=0" match_minor0)
+  if (EXISTS "${_OPENSSL_INCLUDE_DIR}/../VERSION.dat")
+    file (READ "${_OPENSSL_INCLUDE_DIR}/../VERSION.dat" _OPENSSL_VERSION)
+    string (FIND "${_OPENSSL_VERSION}" "MAJOR=3" match_major3)
+    string (FIND "${_OPENSSL_VERSION}" "MINOR=0" match_minor0)
     if (${match_major3} GREATER_EQUAL 0)
       if (${match_minor0} GREATER_EQUAL 0)
         message (STATUS "Sibling OpenSSL 3.0 found.")
@@ -93,6 +125,13 @@ if (OPENSSL_FOUND)
   endif ()
 endif ()
 
+if (OPENSSL_1_1_1_FOUND)
+  set (OPENSSL_VERSION "1.1.1")
+endif ()
+if (OPENSSL_3_0_FOUND)
+  set (OPENSSL_VERSION "3.0")
+endif ()
+
 if (OPENSSL_INCLUDE_DIRS AND NOT OPENSSL_INCLUDE_DIR)
   set (OPENSSL_INCLUDE_DIR ${OPENSSL_INCLUDE_DIRS})
 endif ()
@@ -101,3 +140,6 @@ endif ()
 unset (_OPENSSL_1_1_1_FOUND)
 unset (_OPENSSL_3_0_FOUND)
 unset (_OPENSSL_FOUND)
+unset (_OPENSSL_INCLUDE_DIRS)
+unset (_OPENSSL_INCLUDE_DIR)
+unset (_OPENSSL_VERSION)
